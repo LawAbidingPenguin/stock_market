aa02662f00d886dc6735f48d6cfbf325b84cbcfd
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2020-10-07 14:33:18 +0200

    Add files via upload

---
+++ urls_and_selectors.py
@@ -0,0 +1,28 @@
+# URLS
+
+
+# XPATH SELECTORS
+# Company info
+company_name = '//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1'
+
+# Stock info
+stock_value = '//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]'
+value_change = '//*[@id="quote-header-info"]/div[3]/div[1]/div/span[2]'
+summary = '//*[@id="quote-summary"]'
+
+# Displays current trending stocks
+trending_stock1 = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[1]/td[1]/a'
+trending_stock1_name = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[1]/td[2]'
+
+trending_stock2 = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[2]/td[1]/a'
+trending_stock2_name = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[2]/td[2]'
+
+trending_stock3 = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[3]/td[1]/a'
+trending_stock3_name = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[3]/td[2]'
+
+trending_stock4 = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[4]/td[1]/a'
+trending_stock4_name = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[4]/td[2]'
+
+trending_stock5 = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[5]/td[1]/a'
+trending_stock5_name = '//*[@id="yfin-list"]/div[2]/div/div/table/tbody/tr[5]/td[2]'
+
